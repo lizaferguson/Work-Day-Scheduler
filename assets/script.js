2c8af789f7d11b8c925af8e4c51bc1ab1310ec62
liza <lizaferguson19@gmail.com>
2021-03-09 16:42:27 -0500

    updated index.html and script.js files

--- assets/script.js
+++ assets/script.js
@@ -10,13 +10,92 @@ function displayTime() {
 //calling displayTime function and setting interval to 1 sec (1000ms)
 setInterval(displayTime, 1000);
 
+//setting variable for current hour of the day to pass into function
+var now = moment().hour();
 
-// var plannerEl = document.getElementById('#planner');
+//function to change the background color of the hour row based upon current hour
+//sets class to past=grey, present=orange, future=green 
+function hoursColor() {
+  if (now > 9) {
+    $('#row9').addClass('past');
+  } else if (now >= 9 && now < 10) {
+    $('#row9').addClass('present');
+  } else if (now < 9) {
+    $('#row9').addClass('future');
+  }
 
-// for (hour = 1, hour < 9, hour++) {
-//   console.log(hour)
-//   plannerEl.
-// }
+  if (now > 10) {
+    $('#row10').addClass('past');
+  } else if (now >= 10 && now < 11) {
+    $('#row10').addClass('present');
+  } else if (now < 10) {
+    $('#row10').addClass('future');
+  }
+
+  if (now > 11) {
+    $('#row11').addClass('past');
+  } else if (now >= 11 && now < 12) {
+    $('#row11').addClass('present');
+  } else if (now < 11) {
+    $('#row11').addClass('future');
+  }
+
+  if (now > 12) {
+    $('#row12').addClass('past');
+  } else if (now >= 12 && now < 13) {
+    $('#row12').addClass('present');
+  } else if (now < 12) {
+    $('#row12').addClass('future');
+  }
+
+  if (now > 13) {
+    $('#row1').addClass('past');
+  } else if (now >= 13 && now < 14) {
+    $('#row1').addClass('present');
+  } else if (now < 13) {
+    $('#row1').addClass('future');
+  }
+
+  if (now > 14) {
+    $('#row2').addClass('past');
+  } else if (now >= 14 && now < 15) {
+    $('#row2').addClass('present');
+  } else if (now < 14) {
+    $('#row2').addClass('future');
+  }
+
+  if (now > 15) {
+    $('#row3').addClass('past');
+  } else if (now >= 15 && now < 16) {
+    $('#row3').addClass('present');
+  } else if (now < 15) {
+    $('#row3').addClass('future');
+  }
+
+  if (now > 16) {
+    $('#row4').addClass('past');
+  } else if (now >= 16 && now < 17) {
+    $('#row4').addClass('present');
+  } else if (now < 16) {
+    $('#row4').addClass('future');
+  }
+
+  if (now > 17) {
+    $('#row5').addClass('past');
+  } else if (now >= 17 && now < 18) {
+    $('#row5').addClass('present');
+  } else if (now < 17) {
+    $('#row5').addClass('future');
+  }
+}
+
+hoursColor();
+
+
+
+
+
+
 
 
 
